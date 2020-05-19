5e4f99c5ae90661fd0de4e1ed196d07de453e157
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-05-19 18:54:09 +0600

    :art: Updating initialize configuration with header
    header contains access control allow origin for all

--- src/api/carViewerApi.js
+++ src/api/carViewerApi.js
@@ -4,6 +4,7 @@ import { BASE_URL } from '../constants';
 const initialize = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
+  headers: { 'Access-Control-Allow-Origin': '*' },
 });
 
 export default initialize;
